4df8cc164fadc42c8f5b464ed4b47983a692f1cd
Michael <Michael.hao25@gmail.com>
2018-03-26 17:11:20 +0800

    add index2.html /add plugs swiper v 3.42

--- js/main.js
+++ js/main.js
@@ -2,6 +2,21 @@ require.config({
 	paths: {
 		'jquery': "jquery",
 		//jquery默认加载
-		"js": "js"
+		"js": "js",
+		'swiper': './swiper'
 	}
+})
+
+require(['swiper'], function(swiper) {
+	var mySwiper = new swiper('.banner .swiper-container', {
+		pagination: '.banner .swiper-pagination',
+		'autoplay': 3000
+	})
+	var acc = new swiper('.case .swiper-container', {
+		pagination: '.case .swiper-pagination',
+		'autoplay': 3000,
+		prevButton: '.case .left-arrow',
+		nextButton: '.case .right-arrow',
+		slidesPerView: 3,
+	})
 })
